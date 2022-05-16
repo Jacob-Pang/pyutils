cd42c991e4a555f826e1ba73aad79b927f89b861
Jacob-Pang <jacobpang2103@gmail.com>
2022-05-16 17:40:06 +0800

    Refactored to src style packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup
+from glob import glob
+from setuptools import setup, find_packages
+from os.path import basename, splitext
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+print(find_packages("src"))
 setup(
     name="pyutils",
     version="1.0",
-    description="Miscellaneous utility modules",
+    description="Utility modules for python projects",
     long_description=long_description,
-    packages=["pyutils"],
-    install_requires=[]
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[
+        splitext(basename(path))[0] for path in glob('src/*.py')
+    ],
+    include_package_data=True,
+    install_requires=[
+        "requests", "PyGithub"
+    ]
 )
