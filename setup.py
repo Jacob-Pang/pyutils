73e95ca361a1869fef9d6995947fae1a53458a00
Jacob-Pang <jacobpang2103@gmail.com>
2022-07-11 14:35:55 +0800

    Edit Setup Dependencies

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "dill", "pandas", "PyGithub", "requests"
+        "cloudpickle", "pandas", "PyGithub", "requests"
     ]
 )
