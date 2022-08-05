251c1671d1df2fc8c98d19637015ee72cb4fee3d
Jacob-Pang <jacobpang2103@gmail.com>
2022-08-05 11:34:31 +0800

    Change authentication updates

--- src/pyutils/database/github_database/__init__.py
+++ src/pyutils/database/github_database/__init__.py
@@ -52,18 +52,33 @@ class GitHubDataBase (GitHubDataNode, DataBase):
         self.add_resident_child_node(memory_node)
 
     def set_access_token(self, access_token: str) -> None:
+        if self.host_database:
+            return self.host_database.set_access_token(access_token)
+
         self.access_token = access_token
 
     def get_user_name(self) -> str:
+        if self.host_database:
+            return self.host_database.get_user_name()
+
         return self.user_name
     
     def get_repo_name(self) -> str:
+        if self.host_database:
+            return self.host_database.get_repo_name()
+
         return self.repository_name
 
     def get_branch(self) -> str:
+        if self.host_database:
+            return self.host_database.get_branch()
+
         return self.branch
 
-    def get_authenticated_user(self) -> AuthenticatedUser:           
+    def get_authenticated_user(self) -> AuthenticatedUser:
+        if self.host_database:
+            return self.host_database.get_authenticated_user()
+
         if self.authenticated_user:
             return self.authenticated_user
         
@@ -75,6 +90,9 @@ class GitHubDataBase (GitHubDataNode, DataBase):
 
     def get_authenticated_repo(self) -> Repository:
         # Returns an authenticated repo
+        if self.host_database:
+            return self.host_database.get_authenticated_repo()
+
         if self.authenticated_repo:
             return self.authenticated_repo
         
@@ -86,6 +104,9 @@ class GitHubDataBase (GitHubDataNode, DataBase):
 
     def get_repository(self) -> Repository:
         # Returns an authenticated repo where possible
+        if self.host_database:
+            return self.host_database.get_repository()
+
         if self.access_token or self.authenticated_user:
             return self.get_authenticated_repo()
 
@@ -95,11 +116,34 @@ class GitHubDataBase (GitHubDataNode, DataBase):
         self.repository = get_repository(self.get_repo_name(), self.get_user_name())
         return self.repository
 
+    def get_authentication_cache(self) -> dict:
+        if self.host_database:
+            return self.host_database.get_authentication_cache()
+
+        return {
+            "access_token": self.access_token,
+            "authenticated_user": self.authenticated_user,
+            "authenticated_repo": self.authenticated_repo,
+            "repository": self.repository
+        }
+
     def destroy_authentication_cache(self) -> None:
+        if self.host_database:
+            return self.host_database.destroy_authentication_cache()
+
         self.access_token = None
         self.authenticated_user = None
         self.authenticated_repo = None
         self.repository = None
+
+    def set_authentication_cache(self, authentication_cache: dict) -> None:
+        if self.host_database:
+            return self.host_database.set_authentication_cache(authentication_cache)
+
+        self.access_token = authentication_cache.get("access_token")
+        self.authenticated_user = authentication_cache.get("authenticated_user")
+        self.authenticated_repo = authentication_cache.get("authenticated_repo")
+        self.repository = authentication_cache.get("repository")
 
     def save_database_memory(self, commit_message: str = '', authenticated_repo: Repository = None,
         save_child_nodes: bool = True, **kwargs) -> None:
@@ -111,6 +155,9 @@ class GitHubDataBase (GitHubDataNode, DataBase):
         if not authenticated_repo:
             authenticated_repo = self.get_authenticated_repo()
         
+        # Destroy authentication caches
+        authentication_caches = dict()
+        authentication_caches[self.data_node_id] = self.get_authentication_cache()
         self.destroy_authentication_cache()
 
         for child_node in self.child_nodes.values():
@@ -120,25 +167,27 @@ class GitHubDataBase (GitHubDataNode, DataBase):
                             authenticated_repo=(authenticated_repo if self.has_resident(child_node) else None),
                             **kwargs)
 
+                authentication_caches[child_node.data_node_id] = child_node.get_authentication_cache()
                 child_node.destroy_authentication_cache()
 
         DataBase.save_database_memory(self, authenticated_repo=authenticated_repo, commit_message=
                 commit_message, **kwargs)
+
+        # Reset authentication caches
+        self.set_authentication_cache(authentication_caches.get(self.data_node_id))
+
+        for child_node in self.child_nodes.values():
+            if isinstance(child_node, GitHubDataBase):
+                child_node.set_authentication_cache(authentication_caches.get(child_node.data_node_id))
 
     def autosave_database_memory(self) -> None:
         pass # Does not perform auto save
 
     def add_resident_child_node(self, data_node: DataNode, relative_dpath: str = '') -> None:
         if isinstance(data_node, GitHubDataBase):
-            data_node.user_name = self.user_name
-            data_node.repository_name = self.repository_name
-            data_node.branch = self.branch
-            data_node.access_token = self.access_token
-            data_node.authenticated_user = self.authenticated_user
-            data_node.authenticated_repo = self.authenticated_repo
-            data_node.repository = self.repository
+            data_node.destroy_authentication_cache()
 
-        DataBase.add_resident_child_node(self, data_node, relative_dpath)
+        return super().add_resident_child_node(data_node, relative_dpath)
 
 if __name__ == "__main__":
     pass
