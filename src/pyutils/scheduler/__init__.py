520c0bae3a3eb7eab66b0d2cfc3e01ff8226da27
Jacob-Pang <jacobpang2103@gmail.com>
2022-08-25 14:57:42 +0800

    Updated MasterProcess Functionalities

--- src/pyutils/scheduler/__init__.py
+++ src/pyutils/scheduler/__init__.py
@@ -2,6 +2,8 @@ from multiprocessing import Process
 from multiprocessing.managers import SyncManager
 
 from pyutils import generate_unique_key
+from pyutils.scheduler.task import Task
+from pyutils.scheduler.resource import Resource, ResourceProxy
 from pyutils.scheduler.task_manager import TaskManager
 from pyutils.scheduler.worker import Worker
 
@@ -20,6 +22,21 @@ class MasterProcess (Worker):
         )
 
         Worker.__init__(self, "__MASTER__", master_process_state, None)
+
+    def register_task(self, task: Task, timestamp: float = None) -> None:
+        task_manager = self.master_process_state.task_manager
+
+        with task_manager.semaphore:
+            task_manager.register_task(task, timestamp)
+
+    def register_resource(self, resource: Resource, sync_manager: SyncManager) -> None:
+        self.register_resource_proxy(resource.create_proxy(sync_manager))
+
+    def register_resource_proxy(self, resource_proxy: ResourceProxy) -> None:
+        task_manager = self.master_process_state.task_manager
+
+        with task_manager.semaphore:
+            task_manager.register_resource(resource_proxy)
 
     def spawn_worker_process(self) -> Process:
         worker_key = generate_unique_key()
