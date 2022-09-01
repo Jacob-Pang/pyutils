68f3c6338d960e01b765f21e52040cd569633afc
Jacob-Pang <jacobpang2103@gmail.com>
2022-09-01 22:27:48 +0800

    Remove UUID

--- src/pyutils/__init__.py
+++ src/pyutils/__init__.py
@@ -1,11 +1,2 @@
-import time
-
-_KEYS = 0
-
-def generate_unique_key(prefix: str = "", suffix: str = "") -> str:
-    global _KEYS
-    _KEYS = (_KEYS + 1) % 100
-    return f"{prefix}{int(time.time())}{_KEYS:02}{suffix}"
-
 if __name__ == "__main__":
     pass
