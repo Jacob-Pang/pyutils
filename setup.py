b5c79bcc8d70ffa1f04f4a6eb1836c28b81be622
Jacob-Pang <jacobpang2103@gmail.com>
2022-09-10 15:18:57 +0800

    Testing

--- setup.py
+++ setup.py
@@ -4,22 +4,10 @@ from glob import glob
 from setuptools import Extension
 from setuptools import setup, find_packages
 from os.path import basename, join, splitext
+from Cython.Build import cythonize
 
 with open("README.md", 'r') as readme:
     long_description = readme.read()
-
-# Cython modules
-ext_modules = [
-    Extension("cyutils.vector_as_numpy", sources=[
-            join("src", "pyutils", "cyutils", "vector_as_numpy.pyd"),
-            join("src", "pyutils", "cyutils", "vector_as_numpy.pyx")
-        ])
-]
-
-# Set to Python3
-for ext_module in ext_modules:
-    ext_module.cython_directives = {"language_level": "3"}
-
 
 setup(
     name="pyutils",
@@ -42,5 +30,10 @@ setup(
         "selenium==4.2"
     ],
     include_dirs=[np.get_include()],
-    ext_modules=ext_modules
+    ext_modules=cythonize([
+            Extension("cyutils.vector_as_numpy", sources=[join("src", "pyutils",
+                    "cyutils", "vector_as_numpy")])
+        ],
+        compiler_directives = {"language_level": "3"}
+    )
 )
