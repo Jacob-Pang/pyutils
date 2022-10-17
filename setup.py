e2cf0ece20e085e9aa56eb692d750afaa0d92b17
Jacob-Pang <jacobpang2103@gmail.com>
2022-10-17 23:53:19 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from glob import glob
 from setuptools import Extension
 from setuptools import setup, find_packages
 from os.path import basename, join, splitext
-from Cython.Build import cythonize
 
 with open("README.md", 'r') as readme:
     long_description = readme.read()
@@ -27,14 +26,5 @@ setup(
         "requests",
         "rpa",
         "selenium==4.2"
-    ],
-    ext_modules=cythonize([
-            Extension("pyutils.cyutils.array_as_numpy", sources=[join("src", "pyutils",
-                    "cyutils", "array_as_numpy.pyx")])
-        ],
-        compiler_directives = {"language_level": "3"},
-    ),
-    include_dirs=[
-        np.get_include()
     ]
 )
