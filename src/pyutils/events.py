aa2be0f1e85a7977ce0e1d4fd38274dccc1d1935
Jacob-Pang <jacobpang2103@gmail.com>
2022-12-12 13:54:48 +0800

    Added kwargs option for wait event

--- src/pyutils/events.py
+++ src/pyutils/events.py
@@ -1,12 +1,25 @@
 import asyncio
 import time
 
-def wait_for(predicate: callable, timeout: float = None, revaluate_delay: float = .5) -> bool:
+def wait_for(predicate: callable, timeout: float = None, revaluate_delay: float = .5, **kwargs) -> any:
+    """ Busy waiting for event trigger captured by the predicate.
+
+    params:
+        predicate (callable): The predicate function to capture the occurrence of the event.
+        timeout (float, opt): The maximum time to wait for the event to occur. Waits indefinitely if
+                timeout is not specified.
+        revaluate_delay (float, opt): The frequency in seconds at which to re-evaluate the predicate.
+        **kwargs: Any keyword arguments to pass into the predicate function.
+    
+    returns:
+        predicate_value (any): The return value of the predicate. Returns None if the predicate
+                function has not been invoked (timeout is set to negative.)
+    """
     start_time = time.time()
-    predicate_value = False
+    predicate_value = None
 
     while not timeout or time.time() < start_time + timeout:
-        predicate_value = predicate()
+        predicate_value = predicate(**kwargs)
 
         if predicate_value:
             break
@@ -15,12 +28,25 @@ def wait_for(predicate: callable, timeout: float = None, revaluate_delay: float 
     
     return predicate_value
 
-async def await_for(predicate: callable, timeout: float = None, revaluate_delay: float = .5) -> bool:
+async def await_for(predicate: callable, timeout: float = None, revaluate_delay: float = .5, **kwargs) -> any:
+    """ Async busy waiting for event trigger captured by the predicate.
+
+    params:
+        predicate (callable): The predicate function to capture the occurrence of the event.
+        timeout (float, opt): The maximum time to wait for the event to occur. Waits indefinitely if
+                timeout is not specified.
+        revaluate_delay (float, opt): The frequency in seconds at which to re-evaluate the predicate.
+        **kwargs: Any keyword arguments to pass into the predicate function.
+    
+    returns:
+        predicate_value (any): The return value of the predicate. Returns None if the predicate
+                function has not been invoked (timeout is set to negative.)
+    """
     start_time = time.time()
-    predicate_value = False
+    predicate_value = None
 
     while not timeout or time.time() < start_time + timeout:
-        predicate_value = predicate()
+        predicate_value = predicate(**kwargs)
 
         if predicate_value:
             break
